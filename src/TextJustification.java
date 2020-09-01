4f837898ae3c186916e70d577a95716a61b8f176
Steve Thompson <stevethompsonnyc@gmail.com>
2020-08-31 23:17:28 -0400

    Fixed padding rules to align with acceptance criteria that left justified lines be padded anyway

--- src/TextJustification.java
+++ src/TextJustification.java
@@ -28,7 +28,6 @@ public class TextJustification {
                 }
             }
 
-
             String lineToAdd = new String();
             lineToAdd = returnPaddedLine(nextLine, currentLineLength, maxWidth, currentWordIndex >= words.length);
             System.out.println(lineToAdd);
@@ -43,50 +42,65 @@ public class TextJustification {
 
         // We need to add at least 1 space delimiter for all but the last word
         int numOfDelimiters = lineWords.size() - 1;
+        if (numOfDelimiters < 1) { numOfDelimiters = 1; }
 
         int paddedWidth = currentWidth;
         int paddingNeeded = maxWidth - currentWidth;
         int avgSpacesPerDelimiter = paddingNeeded/numOfDelimiters;
         int extraPadCounter = paddingNeeded - (avgSpacesPerDelimiter * numOfDelimiters);
+        String padding = new String();
 
-        if (!lastLine) {
-            // for all but the last line,
-            // add padding between all inner words.
-            for (int i = 0; i < lineWords.size() - 1; i++) {
-                String padding = new String();
-                paddedLine.append(lineWords.get(i) + " ".repeat(avgSpacesPerDelimiter + extraPadCounter));
-                if (extraPadCounter > 0) {
-                    extraPadCounter--;
-                } else {
-                    extraPadCounter = 0;
-                }
+        avgSpacesPerDelimiter = paddingNeeded/numOfDelimiters;
+        extraPadCounter = paddingNeeded - (avgSpacesPerDelimiter * numOfDelimiters);
+
+        // for all but the last line,
+        // add padding between all inner words.
+        for (int i = 0; i < lineWords.size() - 1; i++) {
+            if (lastLine) {
+                padding = " ";
+            } else {
+                padding = " ".repeat(avgSpacesPerDelimiter + extraPadCounter);
             }
-            // Add last word without padding
-            paddedLine.append(lineWords.get(lineWords.size() - 1));
-        } else {
-            // Only add one space beteen words
-            // Add extra padding to end
 
+            paddedLine.append(lineWords.get(i) + padding);
+            if (extraPadCounter > 0) {
+                extraPadCounter--;
+            } else {
+                extraPadCounter = 0;
+            }
         }
 
+        // Last line or lines with one word should have needed padding appended
+
+        paddedLine.append(lineWords.get(lineWords.size() - 1));
+
+        String  lastPadding = new String();
+        if (lastLine  || lineWords.size() == 1) {
+            lastPadding = " ".repeat(maxWidth - paddedLine.length());
+        } else {
+            lastPadding = "";
+        }
+        paddedLine.append(lastPadding);
         return paddedLine.toString();
     }
 
     public static void main(String[] args) {
-        //String[] words = new String[] {"This", "is", "an", "example", "of", "text", "justification."};
+        String[] words = new String[] {"What","must","be","acknowledgment","shall","be"};
+//        String[] words = new String[] {"This", "is", "an", "example", "of", "text", "justification."};
 
 //        String[] words = new String[] {"This", "is", "an", "example", "of", "text", "justification",
 //           "and", "it", "goes", "on", "for", "quite", "a", "while"};
 
+        //
         //String[] words = new String[] {"Hello", "world"};
 
 //        String[] words = new String[] {"Hello"};
 
-//        int maxWidth = 16;
+        int maxWidth = 16;
 
-        String[] words = new String[] {"Science","is","what","we","understand","well","enough","to","explain",
-                "to","a","computer.","Art","is","everything","else","we","do"};
-        int maxWidth = 20;
+//        String[] words = new String[] {"Science","is","what","we","understand","well","enough","to","explain",
+//                "to","a","computer.","Art","is","everything","else","we","do"};
+//        int maxWidth = 20;
 
         List<String> result = new TextJustification().fullJustify(words, maxWidth);
         System.out.println("Final result:");
