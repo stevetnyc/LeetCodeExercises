967052cb46efcd7ef47129296072cb65d1aa9279
Steve Thompson <stevethompsonnyc@gmail.com>
2025-02-24 21:13:49 -0500

    Optimized using solution found online

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -1,41 +1,47 @@
 public class LongestPalindromicSubstring {
 
     public String longestPalindrome(String s) {
-            int revIndex = s.length();
-            if (revIndex == 0 || revIndex > 1000) return "";
-            String longest = s.substring(0, 1);
+            int endIdx = s.length();
+            int maxLen = 1;
+            int start = 0;
+            if (endIdx == 0 || endIdx > 1000) return "";
 
             for (int i = 0; i < s.length(); i++) {
-                while (i < revIndex - 1) {
-                    if (isPalindrome(s.substring(i, revIndex))) {
-                        if (s.substring(i, revIndex).length() > longest.length()) {
-                            longest = s.substring(i, revIndex);
+                for (int odd = 0; odd <= 1; odd++) {
+                    int low = i;
+                    int high = i + odd;
+                    while (low >= 0 && high < endIdx && s.charAt(low) == s.charAt(high)) {
+                        int currLen = high - low + 1;
+                        if (currLen > maxLen) {
+                            start = low;
+                            maxLen = currLen;
                         }
+                        low--;
+                        high++;
                     }
-                    revIndex--;
                 }
-                revIndex = s.length();
+
             }
-            return longest;
+        return s.substring(start, start + maxLen);
     }
 
-    public Boolean isPalindrome(String s) {
-        return s.equals(reverseString(s));
-    }
-    public String reverseString(String s) {
-        StringBuilder revString = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            revString.append(s.charAt(i));
-        }
-        return revString.toString();
-    }
+//    public Boolean isPalindrome(String s) {
+//        return s.equals(reverseString(s));
+//    }
+//    public String reverseString(String s) {
+//        StringBuilder revString = new StringBuilder();
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            revString.append(s.charAt(i));
+//        }
+//        return revString.toString();
+//    }
 
     public static void main(String[] args) {
 //        String s = "babad";
 //        String s = "cbbd";
-//        String s = "zzazzmadamimadamx";
+        String s = "zzazzmadamimadamx";
 //        String s = "ac";
-        String s = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
+//        String s = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
         System.out.println("String: " + s);
         System.out.println(new LongestPalindromicSubstring().longestPalindrome(s));
     }
