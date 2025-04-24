387e538be1981f2c67cc1d482155d9bb4fc5a94d
Steve Thompson <stevethompsonnyc@gmail.com>
2025-04-24 16:32:04 -0400

    DP exercises

---
+++ src/DPExercises.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DPExercises {
+
+    static int numDecodings(String s) {
+
+        int possVals = 0;
+
+        int len = s.length();
+        if (s.charAt(0) == '0') return 0;
+
+        int[] dp = new int[len + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < len + 1; i++) {
+            int digit = Integer.valueOf(s.substring(i - 1, i));
+            if (digit != 0) {
+                dp[i] += dp[i - 1];
+            }
+
+            digit = Integer.valueOf(s.substring(i - 2, i));
+            if (digit >= 10 && digit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+      return dp[len];
+    }
+
+
+    public static void main(String[] args) {
+
+        String code = "06";
+        System.out.println(numDecodings(code));
+    }
+}
