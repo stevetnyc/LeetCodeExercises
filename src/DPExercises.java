8c2cf5c5df50a0b0970389fbaea14f68a7c66562
Steve Thompson <stevethompsonnyc@gmail.com>
2025-04-30 16:31:14 -0400

    More DP Exercises

--- src/DPExercises.java
+++ src/DPExercises.java
@@ -43,6 +43,27 @@ public class DPExercises {
 
     }
 
+    public int lengthOfLIS(int[] nums) {
+//        nums = [8,2,4,3,6,12]
+
+        int result = 0;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+           for (int j = 0; j < i; j++) {
+               if (nums[i] > nums[j]) {
+                       dp[i] = Math.max(dp[i], dp[j] + 1);
+               }
+           }
+        }
+
+
+
+        return result;
+    }
 
     public static void main(String[] args) {
 
