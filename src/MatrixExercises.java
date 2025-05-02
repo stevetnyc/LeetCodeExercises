bc9c48ad5e588bda002c6ddd16901a78e2c0a32d
Steve Thompson <stevethompsonnyc@gmail.com>
2025-05-02 15:26:44 -0400

    Added rotate()

--- src/MatrixExercises.java
+++ src/MatrixExercises.java
@@ -66,12 +66,35 @@ public class MatrixExercises {
     }
 
 
+    public static void rotate(int[][] matrix) {
+//        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+//        Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = i; j < matrix.length; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length/2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
+                matrix[i][matrix[i].length - 1 - j] = tmp;
+            }
+        }
+        Utils.printArr(matrix);
+    }
 
     public static void main(String[] args) {
 
 //        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        Utils.printArr(spiralOrder(arr));
+//        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+//        Utils.printArr(spiralOrder(arr));
+//        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        rotate(matrix);
 
     }
 
