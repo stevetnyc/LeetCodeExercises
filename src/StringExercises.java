6a6f8fa29d864082a15f2338931723954ada7a86
Steve Thompson <stevethompsonnyc@gmail.com>
2025-05-03 14:53:45 -0400

    Added multiply() function

--- src/StringExercises.java
+++ src/StringExercises.java
@@ -70,11 +70,46 @@ public class StringExercises {
         return result;
     }
 
+    public static String multiply(String num1, String num2) {
+        // num1 = 12; num2 = 16
+        if ("0".equals(num1) || "0".equals(num2)) return "0";
+        String result = "";
+
+        int[] productArr = new int[num1.length() + num2.length()];
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+
+                productArr[i+j+1] += digit1 * digit2;
+            }
+        }
+
+        for (int i = productArr.length - 1; i > 0; i--) {
+            productArr[i - 1] += productArr[i] / 10;
+            productArr[i] = productArr[i] % 10;
+        }
+
+        int start = productArr[0] == 0 ? 1 : 0;
+
+        for (int i = start; i < productArr.length; i++) {
+            result += productArr[i];
+        }
+
+        return result;
+
+    }
+
     public static void main(String[] args) {
 
-        int n = 3;
 
-        Utils.printList(generateParenthesis(n));
+        String num1 = "15", num2 = "100";
+        System.out.println(multiply(num1, num2));
+
+//        int n = 3;
+//
+//        Utils.printList(generateParenthesis(n));
 
 //        String s = "PAYPALISHIRING";
 //        int numRows = 4;
